ee2c48eb740973016a08ec43ab72f21ee137e415
AlexKluchkov <alexkluchkov@gmail.com>
2026-06-02 20:56:30 +0300

    Add db

--- main.py
+++ main.py
@@ -32,6 +32,14 @@ app.add_middleware(
 app.mount("/static", StaticFiles(directory="static"), name="static")
 #templates = Jinja2Templates(directory="view")
 
+from core.parsing_data_from_website import parsing_data_from_website
+from db.get_db import get_db
+
+@app.on_event("startup")
+async def on_startup():
+    db = next(get_db())
+    await parsing_data_from_website(db)
+
 app.include_router(main_router.router)
 app.include_router(about.router)
 
