9b1471b0bb0d8580abb59f02438a1c0a079c4c32
AlexKluchkov <alexkluchkov@gmail.com>
2026-06-02 21:14:36 +0300

    delate

--- main.py
+++ main.py
@@ -35,10 +35,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 from core.parsing_data_from_website import parsing_data_from_website
 from db.get_db import get_db
 
-@app.on_event("startup")
-async def on_startup():
-    db = next(get_db())
-    await parsing_data_from_website(db)
 
 app.include_router(main_router.router)
 app.include_router(about.router)
