4728a4f262a4c7826851cf89b979cca269fc6d0f
AlexKluchkov <alexkluchkov@gmail.com>
2026-06-02 21:18:51 +0300

    database add

--- main.py
+++ main.py
@@ -33,8 +33,13 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 #templates = Jinja2Templates(directory="view")
 
 from core.parsing_data_from_website import parsing_data_from_website
+
 from db.get_db import get_db
 
+@app.on_event("startup")
+async def on_startup():
+    db = next(get_db())
+    await parsing_data_from_website(db)
 
 app.include_router(main_router.router)
 app.include_router(about.router)
