9a92306871169b8d738e3c433b4efda574970ee8
AlexKluchkov <alexkluchkov@gmail.com>
2026-06-03 22:57:35 +0300

    add database

--- routers/on_startup.py
+++ routers/on_startup.py
@@ -1,8 +1,12 @@
 from core.parsing_data_from_website import parsing_data_from_website
 
 from db.get_db import get_db
+from fastapi import APIRouter
 
-@app.on_event("startup")
+router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+@router.geton_event("startup")
 async def on_startup():
     db = next(get_db())
     await parsing_data_from_website(db)
