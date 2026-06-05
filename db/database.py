d9303238fedf143676d38142e9d98d878a14c87c
AlexKluchkov <alexkluchkov@gmail.com>
2026-06-05 16:31:54 +0300

    add about_producrt

--- db/database.py
+++ db/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-#DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql://postgres:11111@localhost:5432/tssdatabase"
+#DATABASE_URL = f"postgresql://postgres:11111@localhost:5432/tssdatabase"
 
 engine = create_engine(DATABASE_URL)
 
